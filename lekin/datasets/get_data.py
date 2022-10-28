29378472d45e589753f8c08d8e59c4ba6ae1a0a9
Longxing <tanlongxing888@163.com>
2022-10-28 13:12:09 +0800

    update get_data

--- lekin/datasets/get_data.py
+++ lekin/datasets/get_data.py
@@ -3,10 +3,11 @@
 import json
 import logging
 
-# def get_data(name):
-#     machine_list = []
-#     route_list = []
-#     if name == "simple":
-#         pass
-#
-#     return
+
+def get_data(name):
+    # machine_list = []
+    # route_list = []
+    if name == "simple":
+        pass
+
+    return
