916f319d0bc9f2516b5053d18306c6b6aa6dfcba
LongxingTan <tanlongxing888@163.com>
2022-11-25 15:05:08 +0800

    upda

--- lekin/__init__.py
+++ lekin/__init__.py
@@ -1,12 +1,12 @@
 from lekin.datasets.get_data import get_data
 from lekin.lekin_struct.job import Job
 from lekin.lekin_struct.machine import Machine
-from lekin.lekin_struct.operation import Task
+from lekin.lekin_struct.operation import Operation
 from lekin.lekin_struct.route import Route
 from lekin.scheduler import Scheduler
 from lekin.solver.dispatching_rules import Rules
 from lekin.solver.heuristics import Heuristics
 
-__all__ = ["Job", "Machine", "Route", "Task", "Scheduler", "get_data"]
+__all__ = ["Job", "Machine", "Route", "Operation", "Scheduler", "get_data"]
 
 __version__ = "0.0.0"
