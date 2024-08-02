acd92f3bff9402edb7cac4429a717d04c027d802
LongxingTan <tanlongxing888@163.com>
2024-08-02 17:23:05 +0800

    fix: example

--- lekin/solver/config.py
+++ lekin/solver/config.py
@@ -1,10 +1,11 @@
 class SolverConfig:
-    def __init__(self):
-        self.entity_selector = None
-        self.move_selector = None
-        self.termination = None
+    def __init__(self, entity_selector=None, move_selector=None, termination=None):
+        self.entity_selector = entity_selector
+        self.move_selector = move_selector
+        self.termination = termination
 
 
 class TerminationConfig:
-    def __init__(self, seconds_spent_limit):
+    def __init__(self, seconds_spent_limit=None, max_iterations=None):
         self.seconds_spent_limit = seconds_spent_limit
+        self.max_iterations = max_iterations
